e0da9bef35939913440a90e5a28a16973b283264
kstipanov <stipanov@stanford.edu>
2016-01-28 14:57:20 -0800

    test

---
+++ class28.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+print("hello there!")
+print("type in an adddress:")
+input() = address_string
+
+ENDPOINT_URL = 'https://maps.googleapiscom/maps/api/geocode/json'
+XTHING = '?address='
+
+FINAL_URL = ENDPOINT_URL + XTHING + address_string
+resp = requests.get(FINAL_URL)
+
+data = json.loads(resp.text)
+
+for r in data['results']:
+	xlist = []
+	xlist.append(r['formatted_address'])
+	xlist.append(r['geometry']['location']['lng'])
+	xlist.append(r['geometry']['location']['lat'])
